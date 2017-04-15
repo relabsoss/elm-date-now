215d4d53ccb6b1d22791e4c3ba572d2169e3bec2
vt <vturchaninov@gmail.com>
2017-04-15 15:15:53 +0300

    Add documentation

--- src/Date/Now.elm
+++ src/Date/Now.elm
@@ -1,7 +1,11 @@
 module Date.Now exposing (now)
+{-| Elm package to get current date without Task
+-}
 
 import Date
 import Native.DateNow
 
+{-| Return current Date
+-}
 now : Date.Date
 now = Native.DateNow.now Nothing
